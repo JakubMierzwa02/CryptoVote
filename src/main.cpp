6a5952d3e86f2c77bd5ff29b33f11e4a3fd96969
JakubMierzwa02 <kontakt.jakubmierzwa@gmail.com>
2024-05-21 03:29:53 -0400

    Added demonstration of functionalities in main [#20]

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,16 @@
 #include "Block.h"
 #include "Blockchain.h"
 #include "User.h"
+#include "Vote.h"
+#include "VoteFactory.h"
+#include "VotingToken.h"
+#include "Wallet.h"
 
 int main()
 {
-    User user(1, "Personal data");
+    User user(1, "Personal Data");
     user.registerUser();
+
     if (user.login())
     {
         Wallet& wallet = user.getWallet();
@@ -24,6 +29,18 @@ int main()
         {
             std::cout << transaction << std::endl;
         }
+
+        auto token = VoteFactory::createVotingToken(10);
+        token->approve();
+        std::cout << "Voting token value: " << token->getValue() << ", valid: " << token->isValid() << std::endl;
+
+        auto vote = VoteFactory::createVote("Candidate A");
+        vote->castVote();
+        std::cout << "Vote choice: " << vote->getChoice() << std::endl;
+
+        Blockchain& blockchain = Blockchain::getInstance();
+        bool isChainValid = blockchain.verifyChain();
+        std::cout << "Blockchain valid: " << (isChainValid ? "true" : "false") << std::endl;
 
         user.logout();
     }
